243c97d18bd22d2b6d398844ddbfb602b84d74a5
nishalinimm-ship-it <nishalinim.m@gmail.com>
2025-11-27 16:32:48 +0530

    design changes

--- client/src/app/upload/upload.component.ts
+++ client/src/app/upload/upload.component.ts
@@ -3,12 +3,14 @@ import { Component } from '@angular/core';
 import { FileUploadModule } from 'primeng/fileupload';
 import { CommonModule } from '@angular/common';
 
+
 @Component({
   selector: 'app-uploadfile',
   standalone: true,
   imports: [CommonModule, FileUploadModule],
   templateUrl: './upload.component.html',
-  styleUrls: ['./upload.component.css']
+  styleUrls: ['./upload.component.css'],
+   
 })
 export class UploadComponent {
     uploadedFiles: any[] = []; // store uploaded files
